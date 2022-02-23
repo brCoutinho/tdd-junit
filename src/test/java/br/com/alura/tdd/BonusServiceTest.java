3dcbc31f1b824c9f20df67314da24d7b7bd6ab20
Thiago Coutinho <thiago.coutinho@summit-bra.com>
2022-02-23 16:42:53 -0300

    first commit

---
+++ src/test/java/br/com/alura/tdd/BonusServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.alura.tdd;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class BonusServiceTest {
+
+    @Test
+    @DisplayName("Caso o cálculo de bônus com base no salário for maior que mil, funcionário não receberá nada")
+    public void bonusZeroParaSalariosAltos(){
+
+    }
+}
