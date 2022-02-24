7b8ccf8bba81ef75f109e0500dc4aa5480c6026b
Thiago Coutinho <thcoutinho1997@gmail.com>
2022-02-24 12:36:30 -0300

    refatoração ifs

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -11,19 +11,10 @@ public class ReajusteService {
         if (desempenho == Desempenho.A_DESEJAR){
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
             funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.BOM){
+        } else if (desempenho == Desempenho.BOM){
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
             funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.BOM){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.OTIMO){
+        } else {
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
             funcionario.reajustarSalario(reajuste);
         }
